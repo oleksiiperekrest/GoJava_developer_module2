6f4c933061f6a7745c585322e814115f826f42b9
simurashka <simurashka@gmail.com>
2018-01-14 21:05:53 +0200

    improved view.Menu.editMenu

--- src/main/java/view/Menu.java
+++ src/main/java/view/Menu.java
@@ -1,9 +1,10 @@
 package view;
 
+import model.Entity;
+
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
 
 public class Menu
@@ -29,17 +30,35 @@ public class Menu
         System.out.println("0. Return to previous menu");
     }
 
+    /**
+     * Prints out edit menu for specified entity, like:
+     * "    1. FirstClassField
+     *      2. SecondClassField
+     *      ...
+     *      5. Show current %className
+     *      6. Save current %className
+     *      0. Return to previous menu."
+     * @param clazz class of entity. Meant to be heir of model.Entity
+     *
+     * @return i index of Save menu item, returned for filtering user choice of menu options
+     * Only integers between 0 and i would be valid choice representing a menu item.
+     */
     public static int editMenu(Class clazz) {
         System.out.println("Choose a field to edit:");
         String[] fields = classFieldsNames(clazz);
         int i = 0;
-        for ( ; i < fields.length; i++) {
+        for ( ; i < fields.length; i++) { //List all class fields paired with a counting number (up from 1)
             System.out.println(i+1 + ". " + fields[i]);
         }
-        System.out.println((i+1) + ". Show current " + clazz.getName().split("model.")[1].toLowerCase());
-        System.out.println((i+2) + ". Save current " + clazz.getName().split("model.")[1].toLowerCase());
+
+        i++; //next menu entry after last class field
+        System.out.println(i + ". Show current " + clazz.getName().split("model.")[1].toLowerCase());
+
+        i++;
+        System.out.println(i + ". Save current " + clazz.getName().split("model.")[1].toLowerCase());
+
         System.out.println("0. Return to previous menu.");
-        return i+2;
+        return i;
     }
 
     private static String[] classFieldsNames(Class clazz) {
