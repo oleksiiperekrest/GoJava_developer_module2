dffbd2e41a463cbc1c01788e7053b7ff8015ecca
simurashka <simurashka@gmail.com>
2018-01-28 00:23:50 +0200

    done Hibernate

--- src/main/java/Debug.java
+++ src/main/java/Debug.java
@@ -1,17 +1,20 @@
+import dao.DeveloperDAO;
+import dao.hibernate.HibernateDeveloperDAOImpl;
+import dao.jdbc.JdbcDeveloperDAOImpl;
 import model.Developer;
 import model.Entity;
+import view.Show;
 
 import java.lang.reflect.Field;
+import java.sql.SQLException;
+import java.util.List;
 
 public class Debug {
-    public static void main(String[] args) {
-        Developer developer = new Developer();
-        Entity entity = new Entity();
-        entity.setId(1);
-        Field[] fields = entity.getClass().getDeclaredFields();
+    public static void main(String[] args) throws SQLException {
+        DeveloperDAO hibernateDeveloperDAO = new HibernateDeveloperDAOImpl();
 
-        for (Field field : fields) {
-            System.out.println(field.getName());
-        }
+        hibernateDeveloperDAO.deleteById(12);
+        List<Developer> developerList = hibernateDeveloperDAO.getAll();
+        Show.listAll(developerList);
     }
 }
